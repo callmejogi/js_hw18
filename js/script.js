753640d2c55993cc38eb177db9247e63229ded1b
callmejogi <kutafin01@gmail.com>
2021-10-28 13:33:48 +0300

    oneToTwelve

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const arrayNum = [1,2,3];
+//  #1
+
+// const arrNum = [4,5,6];
+
+// arrayNum.concat(arrNum);
+// console.log(arrayNum);
+
+// #2
+
+// arrayNum.reverse();
+// console.log(arrayNum);
+
+// #3
+
+// arrayNum.push(4,5,6);
+// console.log(arrayNum);
+
+// #4
+
+// arrayNum.unshift(4,5,6);
+// console.log(arrayNum);
+
+// #5 #6
+
+// const arr = ['js','css','jq'];
+
+// console.log(arr[0]);
+// console.log(arr[arr.length-1]);
+
+// #7-12
+
+const arr = [1,2,3,4,5];
+
+// console.log(arr.slice(0,3));
+
+// console.log(arr.slice(3,5));
+
+// arr.splice(0,3,1)
+// console.log(arr);
+
+// console.log(arr.slice(1,4));
+
+// arr.splice(0,3,1,2,3,'a','b','c');
+// console.log(arr);
+
+// arr.splice(0,5,1,'a','b',2,3,4,'c',5,'e');
+// console.log(arr);
+
+// #13
+
+
+
+
